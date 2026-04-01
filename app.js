2bd0c60b417669e88056474c36cd597db474a10d
Gabriel07-dev-coder <jg425771@gmail.com>
2026-04-01 01:43:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+import { db } from './firebase-config.js';
+import { doc, updateDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+
+// 1. Função para o celular da ANA (entregador.html)
+export function iniciarRastreamento(id) {
+    if ("geolocation" in navigator) {
+        // Alerta para confirmar que o código começou a rodar no celular
+        alert("GPS Iniciado! Tentando capturar posição..."); 
+
+        navigator.geolocation.watchPosition((position) => {
+            const docRef = doc(db, "entregadores", id);
+            updateDoc(docRef, {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+                status: "online"
+            }).then(() => {
+                console.log("Posição enviada ao Firebase!");
+            }).catch(err => {
+                console.error("Erro ao gravar no banco:", err);
+            });
+        }, (error) => {
+            alert("Erro no GPS: " + error.message);
+        }, { enableHighAccuracy: true });
+    } else {
+        alert("Seu celular não suporta GPS no navegador.");
+    }
+}
+
+// 2. Função para o seu MAPA (index.html)
+export function monitorarEntregador(id, callback) {
+    const docRef = doc(db, "entregadores", id);
+    onSnapshot(docRef, (docSnap) => {
+        if (docSnap.exists()) {
+            callback(docSnap.data());
+        }
+    });
+}
---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCxSHASI8Ve9evqhKfd@bnTfXagqLIunM",
+    authDomain: "hamburgueria-tracker.firebaseapp.com",
+    projectId: "hamburgueria-tracker",
+    storageBucket: "hamburgueria-tracker.firebasestorage.app",
+    messagingSenderId: "1070466497328",
+    appId: "1:1070466497328:web:74132bd089e63817aea6f5",
+    measurementId: "G-JQYBOEBT4T"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
