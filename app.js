947f574f88a7e221d6f7d9c330817b48eea82044
Gabriel07-dev-coder <jg425771@gmail.com>
2026-04-03 02:39:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-// Inicialização do Mapa Dark Mode
-const map = L.map('map').setView([-25.4614, -49.2275], 13);
+// Remova as tags <script> daqui!
+const baseCoords = L.latLng(-25.4431, -49.2761); 
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '© OpenStreetMap'
-}).addTo(map);
+window.finalizarComKM = function(idDiv, nomeRua, coordsDestino) {
+    const itemSaindo = document.getElementById(idDiv);
+    if (itemSaindo) itemSaindo.remove();
 
-// Faz o mapa ser acessível pelas funções do index.html
-window.map = map;
+    const destino = L.latLng(coordsDestino[0], coordsDestino[1]);
+    const distanciaKM = (baseCoords.distanceTo(destino) / 1000).toFixed(2);
+
+    const historico = document.getElementById('historico-entregas');
+    const card = document.createElement('div');
+    card.className = 'card-historico';
+    card.innerHTML = `
+        <div style="font-size: 13px; color: white;">✅ ${nomeRua}</div>
+        <div style="font-size: 11px; color: #00ced1;">Distância: ${distanciaKM} km</div>
+    `;
+    historico.prepend(card);
+};
