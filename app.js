aef514d3b843d356dcb2727ac620c2c0bf29b66b
Gabriel07-dev-coder <jg425771@gmail.com>
2026-04-03 02:37:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// Inicialização do Mapa com Tema Escuro
+// Inicialização do Mapa Dark Mode
 const map = L.map('map').setView([-25.4614, -49.2275], 13);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-// Exporte o mapa para que o index.html possa usá-lo
+// Faz o mapa ser acessível pelas funções do index.html
 window.map = map;
