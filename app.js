1d0250c3532a3edb91e08288a0823a38e203d9a5
Gabriel07-dev-coder <jg425771@gmail.com>
2026-04-09 17:02:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,25 +5,21 @@ let map;
 let markerEntregador;
 
 function inicializarMapaEmpresa() {
-    // Inicializa o mapa limpo, sem marcadores na praça
     map = L.map('map', { zoomControl: false }).setView([-25.4351, -49.2786], 15);
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png').addTo(map);
 
-    // Escuta a localização REAL do entregador no Firestore
-    onSnapshot(doc(db, "rastreio", "entregador_1"), (doc) => {
-        const data = doc.data();
+    onSnapshot(doc(db, "rastreio", "entregador_1"), (docSnap) => {
+        const data = docSnap.data();
         if (data) {
             const novaPos = [data.lat, data.lng];
-
             if (!markerEntregador) {
-                // Cria o marcador móvel apenas quando o sinal chega (sem nomes fixos)
                 markerEntregador = L.circleMarker(novaPos, {
                     radius: 10, fillColor: "#00bcd4", color: "white", weight: 3, fillOpacity: 1
                 }).addTo(map);
             } else {
                 markerEntregador.setLatLng(novaPos);
             }
-            map.panTo(novaPos); // O mapa segue você em tempo real
+            map.panTo(novaPos);
         }
     });
 }
