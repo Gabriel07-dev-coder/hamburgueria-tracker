b3b6ad20b8e76bf639d53fde2b74654956f5ccba
Gabriel07-dev-coder <jg425771@gmail.com>
2026-04-10 01:08:56 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxSHASI8Ve9evqhKfd-bnTfXagqLIunM",
+    apiKey: "AIzaSyCxSHASI8Ve9evqhKfd-bnTfXagqLIunM", // Suas chaves reais aqui
     authDomain: "hamburgueria-tracker.firebaseapp.com",
     projectId: "hamburgueria-tracker",
     storageBucket: "hamburgueria-tracker.firebasestorage.app",
     messagingSenderId: "1070466497328",
-    appId: "1:1070466497328:web:74132bd089e63817aea6f5",
-    measurementId: "G-JQYBOEBT4T"
+    appId: "1:1070466497328:web:74132bd089e63817aea6f5"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app); // EXPORTAÇÃO CORRIGIDA
+export const db = getFirestore(app);
+export const auth = getAuth(app);
